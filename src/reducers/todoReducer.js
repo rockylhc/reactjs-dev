a6276b46f3dc4e5f2db54ac201c2cecd703dfadb
Rocky <rockylhc@gmail.com>
2017-08-16 16:20:35 +0800

    Add missing action for todo

--- src/reducers/todoReducer.js
+++ src/reducers/todoReducer.js
@@ -5,6 +5,16 @@ export default function todoReducer(state=initialState.todos, action){
   switch(action.type){
     case types.LOAD_TODOS_SUCCESS:
       return action.todos;
+    case types.CREATE_TODO_SUCCESS:
+      return [
+        ...state,
+        Object.assign({}, action.todo)
+      ];
+    case types.UPDATE_TODO_SUCCESS:
+      return [
+        ...state.filter(todo=>todo.id !== action.todo.id),
+        Object.assign({}, action.todo)
+      ];
     default:
       return state;
   }
